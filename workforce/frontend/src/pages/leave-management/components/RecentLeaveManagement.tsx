768906738fecb7ecd8d3ffd5e106f21dd77aefc3
FhuAnn <phuandongthap02@gmail.com>
2026-08-21 10:22:32 +0700

    familar the feeling of fontend coding after not practicing within some months

---
+++ workforce/frontend/src/pages/leave-management/components/RecentLeaveManagement.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const RecentLeaveManagement = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default RecentLeaveManagement
---
+++ workforce/frontend/src/pages/leave-management/components/TeamLeaveCalendar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TeamLeaveCalendar = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default TeamLeaveCalendar
